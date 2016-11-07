6f12fa3c401898d716230f05402a70911b436099
Damien Clarke <dxinteractive@gmail.com>
2016-11-07 11:34:57 +1100

    Split jsdoc and flow because they are fighting :(

--- src/typedef/Externals.js
+++ src/typedef/Externals.js
@@ -3,51 +3,10 @@
 import {List} from 'immutable';
 import React from 'react';
 
-
-/**
- * Immutable.js Iterable
- * @typedef Iterable
- * @see https://facebook.github.io/immutable-js/docs/#/Iterable
- */
-
-/**
- * Immutable.js List
- * @typedef List
- * @see https://facebook.github.io/immutable-js/docs/#/List
- */
-
-/**
- * Immutable.js Map
- * @typedef Map
- * @see https://facebook.github.io/immutable-js/docs/#/Map
- */
-
-/**
- * Immutable.js OrderedMap
- * @typedef OrderedMap
- * @see https://facebook.github.io/immutable-js/docs/#/OrderedMap
- */
-
-/**
- * Immutable.js Record
- * @typedef Record
- * @see https://facebook.github.io/immutable-js/docs/#/Record
- */
-
-/**
- * React Component
- * @typedef ReactComponent
- * @see https://facebook.github.io/react/docs/react-component.html
- */
-
-
-
-
 /**
  * Either and Immutable List or a Javascript Array
  */
 type ListOrArray = List<*> | Array<any>;
-
 
 /**
  * Renderable Content
--- src/typedef/Typedefs.js
+++ src/typedef/Typedefs.js
@@ -1,13 +1,4 @@
 /* @flow */
 /* eslint-disable no-unused-vars */
 
-/**
- * A function that builds a higher order React component around another component.
- * These often have some amount of configuration already set by partial function application.
- *
- * @typedef HockApplier
- * @param {ReactComponent} ComposedComponent The component you wish to wrap in a hock.
- * @return {ReactComponent} The higher order component.
- */
-
 type HockApplier = (ComposedComponent: ReactClass<any>) => ReactClass<any>;

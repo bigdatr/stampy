a0915c592fcd34c1013d87652dd88cdb6fa5b77e
Damien Clarke <dxinteractive@gmail.com>
2016-11-18 11:36:25 +1100

    Tests

--- src/util/SpruceClassName-test.js
+++ src/util/SpruceClassName-test.js
@@ -2,9 +2,35 @@ import test from 'ava';
 import SpruceClassName from './SpruceClassName';
 
 test('modifier splitting', tt => {
-    tt.is(SpruceClassName({name: 'Test', modifier:'rad cool'}), 'Test Test-rad Test-cool ');
+    const className = SpruceClassName({name: 'Test', modifier:'rad cool'});
+
+    tt.truthy(
+        className.match(/\bTest\b/) &&
+        className.match(/\bTest-rad\b/) &&
+        className.match(/\bTest-cool\b/)
+    );
 });
 
-test('modifier splitting', tt => {
-    tt.is(SpruceClassName({name: 'Test'}, 'extra'), 'Test extra');
+test('extra classnames params', tt => {
+    const className = SpruceClassName({name: 'Test'}, 'extra');
+    tt.truthy(
+        className.match(/\bTest\b/) &&
+        className.match(/\bextra\b/)
+    );
+});
+
+test('modifier splitting with objects', tt => {
+    const className = SpruceClassName({
+        name: 'Test',
+        modifier: {
+            yes: true,
+            no: false
+        }
+    });
+
+    tt.truthy(
+        className.match(/\bTest\b/) &&
+        className.match(/\bTest-yes\b/) &&
+        !className.match(/\bTest-no\b/)
+    );
 });

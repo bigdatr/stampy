64039afe363bd1adf7fb3b11178839f409999d50
Damien Clarke <dxinteractive@gmail.com>
2017-03-23 15:49:52 +1100

    Add query string hock example

---
+++ example/src/hock/QueryStringHockExample.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {QueryStringHock} from 'stampy';
+
+const Example = (props) => {
+    const {
+        query,
+        setQuery,
+        updateQuery
+    } = props;
+
+    console.log('Props:', props);
+
+    return <div>
+        <button onClick={() => updateQuery({foo: "FOO"})}>Add "foo=FOO" to query string</button>
+        <button onClick={() => updateQuery({foo: "YEAH"})}>Add "foo=YEAH" to query string</button>
+        <button onClick={() => updateQuery({bar: "BAR"})}>Add "bar=BAR" to query string</button>
+        <button onClick={() => updateQuery({bar: null})}>Remove "bar" from query string</button>
+        <button onClick={() => updateQuery({arr: ["A", "B"]})}>Add "arr=A&arr=B" to query string</button>
+        <button onClick={() => setQuery({baz: "BAZ"})}>Replace query string completely with "baz=BAZ"</button>
+        <button onClick={() => setQuery({})}>Clear query string</button>
+    </div>;
+}
+
+Example.propTypes = {
+    setQuery: React.PropTypes.func,
+    updateQuery: React.PropTypes.func
+};
+
+const QueryStringHockExample = QueryStringHock({
+    arrayParams: ['arr']
+})(Example);
+
+export default QueryStringHockExample;

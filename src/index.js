2b3bc1e5af087719eae1a8be2eac6c97a3492632
Damien Clarke <dxinteractive@gmail.com>
2017-04-26 14:39:25 +1000

    Export CollectionUtils

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ export {default as ToggleSet} from './input/toggleSet/ToggleSet';
 
 export {default as SpruceClassName} from './util/SpruceClassName';
 export {default as SpruceComponent} from './util/SpruceComponent';
+export {default as CollectionUtils} from './util/CollectionUtils';
 
 export {default as ElementQueryHock} from './hock/ElementQueryHock';
 export {default as FormHock} from './hock/FormHock';

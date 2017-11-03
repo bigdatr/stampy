afceb48d13ac99f4dffe5c812df0a60535dea24c
Damien Clarke <dxinteractive@gmail.com>
2017-11-03 16:07:09 +1100

    Remove sass

--- scripts/css-base-combined.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-mkdir -p lib &&                                                 \
-find src -name '*-base.scss' |                                  \
-xargs cat > lib/base.scss &&                                    \
-node-sass --include-path node_modules --output-style compressed lib/base.scss lib/base.css
--- scripts/css-default-combined.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-mkdir -p lib &&                                                         \
-find src -name '*-base.scss' |                                          \
-xargs cat > lib/default.scss &&                                         \
-find src -name '*-default.scss' |                                       \
-xargs cat >> lib/default.scss &&                                        \
-node-sass --include-path node_modules --output-style compressed lib/default.scss lib/default.css
